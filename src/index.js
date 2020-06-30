f73a07703f4d6b649d9674dfb38e42cb93a5ebb4
Ryan <santosryan131@gmail.com>
2020-06-30 15:34:36 -0300

    FULANO

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./models');
 
 const authController = require('./controllers/auth');
 
@@ -12,6 +13,8 @@ app.get('/', (req , res)=>{
   return res.json('Api running...');
 });
 
-app.listen(3001, () => { 
-  console.log("Listening on port 3001")
+db.sequelize.sync().then(()=>{
+  app.listen(3001, () => { 
+    console.log("Listening on port 3001")
+  });
 });
